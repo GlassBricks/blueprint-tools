c401c96f4b90d5321131e0f5bed9ffc690581790
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-07-12 22:42:54 -0700

    Remove unused EntityProps

--- blueprint-model/src/main/kotlin/glassbricks/factorio/blueprint/entity/Entity.kt
+++ blueprint-model/src/main/kotlin/glassbricks/factorio/blueprint/entity/Entity.kt
@@ -6,21 +6,14 @@ import glassbricks.factorio.blueprint.json.EntityNumber
 import glassbricks.factorio.blueprint.prototypes.EntityWithOwnerPrototype
 import kotlinx.serialization.json.JsonObject
 
-public interface EntityProps {
-    public val name: String
-    public val position: Position
-    public val direction: Direction
-    public val tags: JsonObject?
-}
-
-public interface Entity : EntityProps {
+public interface Entity {
     public val prototype: EntityWithOwnerPrototype
     public val type: String get() = prototype.type
+    public val name: String get() = prototype.name
 
-    public override val name: String get() = prototype.name
-    public override var position: Position
-    public override var direction: Direction
-    public override var tags: JsonObject?
+    public var position: Position
+    public var direction: Direction
+    public var tags: JsonObject?
 
     public fun toJsonIsolated(entityNumber: EntityNumber): EntityJson
 }
