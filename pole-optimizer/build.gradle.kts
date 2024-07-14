e198aa2b3100dcdfce9c915b06ece24b0b6c79e8
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-07-14 12:45:41 -0700

    Add pole-optimizer module

---
+++ pole-optimizer/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+group = "glassbricks.factorio"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(project(":model"))
+    implementation("com.google.ortools:ortools-java:9.10.4067")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    explicitApi()
+}
