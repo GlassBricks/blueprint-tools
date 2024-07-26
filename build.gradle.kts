cba1006bda515d738985a3e5133208906a766a1e
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-07-26 15:43:46 -0700

    Update kotlin to 2.0.20-Beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
-    kotlin("jvm") version "2.0.0" apply false
-    kotlin("plugin.serialization") version "2.0.0" apply false
+    kotlin("jvm") version "2.0.20-Beta2" apply false
+    kotlin("plugin.serialization") version "2.0.20-Beta2" apply false
 }
 
 repositories {
