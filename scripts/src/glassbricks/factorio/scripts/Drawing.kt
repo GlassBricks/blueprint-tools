165f3bb9bd2eceef6954d553ef401b2a8b56679c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-08-02 13:54:37 -0700

    Merge Visualize.kt into Drawing.kt

--- scripts/src/glassbricks/factorio/scripts/Drawing.kt
+++ scripts/src/glassbricks/factorio/scripts/Drawing.kt
@@ -177,3 +177,18 @@ fun Drawing.drawEntities(entities: Iterable<Spatial>): Drawing = apply {
 }
 
 fun drawEntities(entities: SpatialDataStructure<Spatial>): Drawing = drawingFor(entities).drawEntities(entities)
+
+fun Drawing.drawHeatmap(entities: Map<out Spatial, Double>) {
+    fun getColor(percentile: Double): Color {
+        val hue = 0.5 - percentile * 0.5
+        return Color.getHSBColor(hue.toFloat(), 1.0f, 1.0f)
+    }
+    if (entities.isEmpty()) return
+    val min: Double = entities.values.min()
+    val max: Double = entities.values.max()
+    for ((entity, value) in entities) {
+        val percentile = (value - min) / (max - min)
+        val color = getColor(percentile)
+        draw(entity, color)
+    }
+}
