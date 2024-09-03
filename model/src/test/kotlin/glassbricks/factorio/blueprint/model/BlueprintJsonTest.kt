47ac89156b15480528f13e1d726ab4f0ff3316fc
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-09-03 11:17:31 -0500

    Fix test name

--- model/src/test/kotlin/glassbricks/factorio/blueprint/model/BlueprintJsonTest.kt
+++ model/src/test/kotlin/glassbricks/factorio/blueprint/model/BlueprintJsonTest.kt
@@ -9,7 +9,7 @@ import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class BlueprintJsonModelTest {
+class BlueprintJsonTest {
 
     fun assertBpMatches(
         bp: BlueprintJson,
